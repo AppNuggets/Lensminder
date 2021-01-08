1b277464543ae27533b2ce5f27db810c345d184b
Michał Granda <majcoch98@gmail.com>
2021-01-08 12:09:21 +0100

    Create Settings Activity layout and implement dark mode switch

--- app/src/main/java/com/appnuggets/lensminder/activity/SettingsActivity.java
+++ app/src/main/java/com/appnuggets/lensminder/activity/SettingsActivity.java
@@ -1,8 +1,14 @@
 package com.appnuggets.lensminder.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.SwitchPreference;
 
 import com.appnuggets.lensminder.R;
 
@@ -11,12 +17,47 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        setContentView(R.layout.settings_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.settings, new SettingsFragment())
+                    .commit();
+        }
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                super.onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey);
+            SwitchPreference darkModeSwitch = (SwitchPreference) findPreference("dark_mode");
+            if(null != darkModeSwitch) {
+                darkModeSwitch.setOnPreferenceChangeListener((preference, isVibrateOnObject) -> {
+                    boolean enableDarkMode = !((SwitchPreference)preference).isChecked();
+                    if( true ==  enableDarkMode) {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    }
+                    else {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    }
+                    return true;
+                });
+            }
+        }
+    }
 }
