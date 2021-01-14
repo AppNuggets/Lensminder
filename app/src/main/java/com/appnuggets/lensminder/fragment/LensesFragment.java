8d8a862f60c2c7fcf5b5a9a9cf51c587ec70f1c5
ClaudiaPolaczek <claudia.polaczek@gmail.com>
2021-01-14 21:10:34 +0100

    Fix table layout

--- app/src/main/java/com/appnuggets/lensminder/fragment/LensesFragment.java
+++ app/src/main/java/com/appnuggets/lensminder/fragment/LensesFragment.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
